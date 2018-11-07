c427021dce950b6c9ca689cd11f5d0a392a7bbaf
tcandarli <tcandarli@gmail.com>
2018-11-07 15:19:32 +0100

    script update

--- PostgreSQL_3.sql
+++ PostgreSQL_3.sql
@@ -9,7 +9,7 @@ They want to know the average replacement cost of movies by rating.
 
 select rating, AVG(replacement_cost)
 from film
-group by rating
+group by rating;
 
 /***********************************
 2-Show all employees who work in any one of these department id 90,60, 100, 130, 120
@@ -17,7 +17,7 @@ group by rating
 
 select *
 from employees
-where department_id in (90, 60, 100, 130, 120)
+where department_id in (90, 60, 100, 130, 120);
 
 /************************************
 3-Show all job_id and average salary who work as any of these jobs IT_PROG, SA_REP, FI_ACCOUNT,AD_VP 
@@ -32,23 +32,40 @@ group by job_id;
 4-Show all employees whose last name ends with 'a'
 */
 
-
+select last_name
+from employees
+where last_name like '%a';
 
 /*************************************
 5-List all employees whose last name second letter is 'e'
 */
 
+select last_name
+from employees
+where last_name like '_e%';
 
 /****************************************
 6-Show all records whose last name contains 2 lowercase 'a's.
 */
 
+select last_name
+from employees
+where last_name like '%a%' and last_name like '%s%';
 
 /*****************************************
 7-Display max salary  for each departments
 */
 
+select department_id, max(salary)
+from employees
+group by department_id;
 
 /******************************************
 8-Display total salary for each departments except department_id 50, and where total salary >30000
 */
+
+select department_id, sum(salary)
+from employees
+where department_id != 50
+group by department_id
+having sum(salary) > 30000;
