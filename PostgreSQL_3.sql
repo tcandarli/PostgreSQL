915b0e954c5c73611f97c0d91fb04a77fd4ff1b6
tcandarli <tcandarli@gmail.com>
2018-11-07 14:15:30 +0100

    script 3

---
+++ PostgreSQL_3.sql
@@ -0,0 +1,47 @@
+/*********************************
+* PostgreSQL - 3
+*/
+
+/***********************************
+1-Corporate headquarters is auditing our store! 
+They want to know the average replacement cost of movies by rating.
+*/
+
+select rating, AVG(replacement_cost)
+from film
+group by rating
+
+/***********************************
+2-Show all employees who work in any one of these department id 90,60, 100, 130, 120
+*/
+
+
+
+/************************************
+3-Show all job_id and average salary who work as any of these jobs IT_PROG, SA_REP, FI_ACCOUNT,AD_VP 
+*/
+
+
+/************************************
+4-Show all employees whose last name ends with 'a'
+*/
+
+
+/*************************************
+5-List all employees whose last name second letter is 'e'
+*/
+
+
+/****************************************
+6-Show all records whose last name contains 2 lowercase 'a's.
+*/
+
+
+/*****************************************
+7-Display max salary  for each departments
+*/
+
+
+/******************************************
+8-Display total salary for each departments except department_id 50, and where total salary >30000
+*/
