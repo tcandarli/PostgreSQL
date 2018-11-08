242bdad5876ff90853afee3fffcfad3a543e0f2c
tcandarli <tcandarli@gmail.com>
2018-11-08 13:13:11 +0100

    script 4 update

--- Scripts/PostgreSQL_4.sql
+++ Scripts/PostgreSQL_4.sql
@@ -6,9 +6,9 @@
 1- Display country_id, country name, region id , region name from hr database
 */
 
-SELECT country_id, country_name, c.region_id, region_name
-FROM countries AS "c" INNER JOIN regions AS "r"
-ON c.region_id = r.region_id;
+select country_id, country_name, c.region_id, region_name
+from countries AS "c" inner join regions AS "r"
+on c.region_id = r.region_id;
 
 
 /***********************************
@@ -24,13 +24,19 @@ order by country_name;
 3-Display the first and last name, department name, city, and state province for each employee.
 */
 
-
+select first_name, last_name, department_name, city, state_province
+from employees as "e" 
+inner join departments as "d" on e.department_id = d.department_id
+inner join locations as "l" on d.location_id = l.location_id;
 
 /************************************
 4-Display the first name, last name, department id and department name, for all employees for departments 80 or 40.
 */
 
-
+select first_name, last_name, d.department_id, department_name
+from employees as "e" 
+inner join departments as "d" on e.department_id = d.department_id
+where e.department_id = 80 or e.department_id = 40;
 
 /*************************************
 5-Display employees first name, last name department id and all departments including those where does not have any employee.
